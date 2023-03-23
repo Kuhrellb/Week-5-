3b41a16fc1d911aaa16124580819435e01d18b2f
Kuhrell Brown <kuhrellb17@gmail.com>
2023-03-23 13:23:44 -0400

    commit 3

--- week5Assignment.js
+++ week5Assignment.js
@@ -1,67 +1,67 @@
-class Shoe{
-    constructor(shoeName,shoeSize){
+class Shoe {
+    constructor(shoeName, shoeSize) {
         this.shoeName = shoeName;
         this.shoeSize = shoeSize;
     }
-    describe(){
+    describe() {
         return `These ${this.shoeName} are a size ${this.shoeSize}.`;
     }
 }
-class Brand{
-    constructor(shoeName){
-        this.shoeName = shoeName;
+class Brand {
+    constructor(brand) {
+        this.brand = brand;
         this.shoes = [];
     }
 
-    addShoe(shoe){
-        if (shoe instanceof shoe) {
-            this.shoes.push(shoe);
-            } else {
-                throw new Error(`You can only add an instance of Shoe. Argument is not a shoe: ${shoe}`);
-    }    
-}
-describe() {
-    return `This brand has ${this.shoes.length} shoes.`;
+    addBrand(brand) { // This passes through an instance of our brand and makes sure that only a brand is being passed.
+        if (brand instanceof Brand) {
+            this.brands.push(brand);
+        } else {
+            throw new Error(`You can only add an instance of Shoe. Argument is not a shoe: ${brand}`);
+        }
+    }
+    describe() {
+        return `This brand has ${this.shoes.length} shoes.`;//returns how many shoes each brand has
     }
 }
-class Menu { 
+class Menu {
     constructor() {
-    this.brands = [];
-    this.selectedBrand = null;
+        this.brands = [];
+        this.selectedBrand = null;
     }
-    start(){
-        let selection = this.showMainMenuOptions(); 
+    start() {
+        let selection = this.showMainMenuOptions();
         while (selection != 0) {
-        switch(selection) {
-        case '1' :
-        this.createBrand();
-        break;
-        case '2' :
-        this.viewBrand();
-        break;
-        case '3' :
-        this.deleteBrand();
-        break;
-        case '4' :
-        this.displayBrand();
-        break;
-        default:
-        selection = 0;
-                     }
-        selection = this.showMainMenuOptions(); // This loop Shows the menu options for what i will be able to do with a brand once created. After a selection is chosen the menu will loop back around.
-                }
-        alert ('Leaving Sneaker Seller Menu App!!!'); //This alerts when you exit the loop meaning user has selected 0 or a different option outside the loop.
+            switch (selection) {
+                case '1':
+                    this.createBrand();
+                    break;
+                case '2':
+                    this.viewBrand();
+                    break;
+                case '3':
+                    this.deleteBrand();
+                    break;
+                case '4':
+                    this.displayBrand();
+                    break;
+                default:
+                    selection = 0;
             }
-    showMainMenuOptions(){
-            return prompt(`
+            selection = this.showMainMenuOptions(); // This loop Shows the menu options for what i will be able to do with a brand once created. After a selection is chosen the menu will loop back around.
+        }
+        alert('Leaving Sneaker Seller Menu App!!!'); //This alerts when you exit the loop meaning user has selected 0 or a different option outside the loop.
+    }
+    showMainMenuOptions() {
+        return prompt(`
             0) Exit
             1) Create a new brand
             2) View a brand
             3) Delete a brand
             4) Display all brands
             `);
-        }
-        showBrandMenuOptions(brandInfo){
+    }
+    showBrandMenuOptions(brandInfo) {
         return prompt(`
             0) Back
             1) Add a new shoe
@@ -69,58 +69,62 @@ class Menu {
             -----------------
             ${brandInfo}
             `);
+    }
+    displayBrand() {
+        let brandString = '';
+        for (let i = 0; i < this.brands.length; i++) {      //for loop concatenate the brands together then allows the user to chose from the brands 
+            brandString += i + ') ' + this.brands[i].brand + '\n';
         }
-        displayBrand() {
-            let brandString = '';
-            for (let i = 0; i < this.brands.length; i++) {      //for loop concatenate the brands together then allows the user to chose from the brands 
-            brandString += i+ ') ' + this.brands[i].brand + '\n';
-            }
-            alert(brandString);
-            }
-        createBrand(){    
-        let brand = prompt(`Enter new Brand Name: `);    //Prompts the user to enter a new brand name tehy would like to add
+        alert(brandString);
+    }
+    createBrand() {
+        let brand = prompt(`Enter new Brand Name: `); 
+        console.log(brand)
+        //Prompts the user to enter a new brand name tehy would like to add
         this.brands.push(new Brand(brand));
-        }
-        viewBrand(){
-            let index = prompt("Enter the index of the Brand that you want to view: ");
-            if (index > -1 && index < this.brands.length) {
+    }
+    viewBrand() {
+        let index = prompt("Enter the index of the Brand that you want to view: ");
+        if (index > -1 && index < this.brands.length) {
             this.selectedBrand = this.brands[index];
             let description = 'Brand Name: ' + this.selectedBrand.brand + '\n';
             description += ' ' + this.selectedBrand.describe() + '\n ';
-            
+
             for (let i = 0; i < this.selectedBrand.shoes.length; i++) {
-            description += i + ') ' + this.selectedBrand.shoes[i].shoeName + '-' + this.selectedBrand.shoes[i].shoeSize + '\n'  //for loop goes throw both the shoe and brand arrays and displays the shoes for whichever specific brand the user selects to be displayed.
-                }
-                let selection = this.showBrandMenuOptions(description);
-                switch (selection) {
-                    case '1' :
+                description += i + ') ' + this.selectedBrand.shoes[i].shoeName + '-' + this.selectedBrand.shoes[i].shoeSize + '\n'  //for loop goes throw both the shoe and brand arrays and displays the shoes for whichever specific brand the user selects to be displayed.
+            }
+            let selection = this.showBrandMenuOptions(description);
+            switch (selection) {
+                case '1':
                     this.createShoe();
                     break;
-                    case '2' :
+                case '2':
                     this.deleteShoe();                  //after selecting a brand the app will pprompt the user to either add a shoe to the brand or delete the shoe from a brand.
-                    }
+            }
 
-            }
-            
         }
-        deleteBrand(){
-            let index = prompt('Enter the index of brand you would like to remove: ');
-            if (index > -1 && index < this.brands.length) { this.brands.splice(index,1);
-            }}
-            createShoe(){
-                if (this.selectedBrand) {
-                let shoeName = prompt('Enter name for new shoe: ');
-                let shoeSize = prompt('Enter shoe size: ');
-                this.selectedBrand.shoes.push(new Shoe(shoeName, shoeSize)); //Takes the user input for a new shoe name and the size of the shoe and ads it to the shoes array
-            } else {
-                alert('No brand selected. Please select a brand first.');
-            }
-            deleteShoe()
-                let index = prompt('Enter the index of the shoe that you was sold: ');
-                if (index > -1 && index < this.selectedBrand.shoes.length) { this.selectedBrand.shoes.splice(index,1);
-            
+
+    }
+    deleteBrand() {
+        let index = prompt('Enter the index of brand you would like to remove: ');
+        if (index > -1 && index < this.brands.length) {
+            this.brands.splice(index, 1);
         }
     }
+    createShoe() {
+        if (this.selectedBrand) {
+            let shoeName = prompt('Enter name for new shoe: ');
+            let shoeSize = prompt('Enter shoe size: ');
+            this.selectedBrand.shoes.push(new Shoe(shoeName, shoeSize)); //Takes the user input for a new shoe name and the size of the shoe and ads it to the shoes array
+        } else {
+            alert('No brand selected. Please select a brand first.');
+        }
+    }
+    deleteShoe() {
+        let index = prompt('Enter the index of the shoe that you was sold: ');
+        if (index > -1 && index < this.selectedBrand.shoes.length) { this.selectedBrand.shoes.splice(index, 1); }
+    }//Takes user input to delete the shoes of their selection by selecting the index of the shoe they want to delete
 }
-            let menu = new Menu();
-            menu.start();
+
+let menu = new Menu();
+menu.start();
